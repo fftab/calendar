9377cf59da2886544c4faa170469d9bb31855c98
Florencia Tabarez <fftabarez97@gmail.com>
2020-04-07 00:07:51 -0600

    Code Refactor.

--- script.js
+++ script.js
@@ -7,29 +7,35 @@ $(document).ready(function() {
     let workHours = [09, 10, 11, 12, 13, 14, 15, 16, 17];
 
     // initializing the current tasks stored in the input fields
-    let currentToDos = [];
+    let toDos = [];
 
     // initializing the timeColumn variable (will append time to this column)
     let timeColumn = $("#time-column"); // ERROR - $ not defined?
 
-    // initializing the input-column variable (will append input boxes to this column)
-    let toDoInputColumn = $("#to-do-input-cloumn");
+    // Testing with Console
+    console.log(timeColumn);
 
-    // initializing the submitButtonColumn variable (will append submit buttons to this column)
+    // initializing the input-column variable (will append input boxes to this column)
+    let toDoInputColumn = $("#input-column");
+
+    // Testing with Console
+    console.log(toDoInputColumn);
+
+    // initializing the saveButtonColumn variable (will append submit buttons to this column)
     let saveButtonColumn = $("#save-button-column");
+
+    console.log(saveButtonColumn)
 
     // initializing the display date paragraph (will display info from moment.js as paragraph text)
     let displayDate = $("#currentDay");
 
     // using the moment.js API to retrieve the month day and year in real time
-    // let currentDate = 'https://moment.js.com/moment().format("ll")'; // Month Day Year
+    let currentDate = moment().format("ll"); // Month Day Year
 
-    // let currentTime = 'https://momentjs.com/moment().format("HH")'; // 24Hr Time Used to check time block
+    let currentHour = moment().format("HH"); // 24Hr Time Used to check time block
 
     // Displaying Month Day and Year from Moment
-    // displayDate.text(JSON.stringify(currentDate));
-
-    // current hour moment()
+    displayDate.text(JSON.stringify(currentDate));
 
     // Functions
     // =================================
@@ -37,21 +43,25 @@ $(document).ready(function() {
     // Function to Initialize the Calendar
     function initCalendar() {
 
-        //Grab local storage
-        //arr  = localstorage("",JSON.parse()); // grabs the string and turns it into object
-        // 
+        // Grab local storage
+        // arr  = localstorage("",JSON.parse()); // grabs the string and turns it into object
+
         // Grab toDoList From The Local Storage
-        // toDoList = JSON.parse(localStorage.getItem("tasks"));
+        // toDos = JSON.parse(localStorage.getItem("tasks"));
 
         //Changed the date
         //JQ to grab the ID for the date
 
         // display page
-        //for loop to for arry
+
+        // For Loop to Populate
         for (i = 0; i < workHours.length; i++) {
 
             // Dynamically Create HTML Elements
             // =================================
+
+            // Create Time Label Elements
+
             // Create Rows for Time Labels
             let newTimeRow = $("<div>");
             // Assign Class of Row to New Time Row
@@ -71,12 +81,18 @@ $(document).ready(function() {
 
             // Creating Input Elements
 
+            // Create New Input Row
+            let newInputRow = $("<div>");
+            // Assigning Attribute
+            newInputRow.attr("class", "row");
             // Create New Input Element
             let newInput = $("<textarea>");
             // Assign an ID to newInput elements
-            newInput.attr("class", "row to-do-input");
+            newInput.attr("class", "to-do-input");
             // Append New Input Element to Input Column
-            toDoInputColumn.append(newInput);
+            newInputRow.append(newInput);
+            // append new input row to to do input column
+            toDoInputColumn.append(newInputRow);
 
             // Create Save Buttons
 
@@ -93,24 +109,30 @@ $(document).ready(function() {
 
     }
 
-    // function setWorkHoursClass() {
+    function setWorkHoursClass() {
 
-    // if ( < currentTime) {
+    if (worksHours[i] < currentHour) {
 
-    //     newTimeLabel.attr("class", "past");
+        newTimeLabel.attr("class", "past");
 
-    // }
-    // else if ( == currentTime) {
+    }
+    else if (workHours[i] == currentHour) {
 
-    // newTimeLabel.attr("class", "future");
+        newTimeLabel.attr("class", "present");
 
-    // }
+    }
+    else if (workHours[i] > currentHour){
 
-    // }
+        newTimeLabel.attr("class", "present");
+
+    }
+
+    }
     
-
     // Populate
     initCalendar();
+
+    setWorkHoursClass();
 
     // // Create Click Event Listener for Save Button
     // $("#save-button").onclick(function () {
